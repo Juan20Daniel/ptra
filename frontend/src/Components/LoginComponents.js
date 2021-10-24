1c33a319d4806b00388a6a2b1572eb50363a5156
JuanDanie1 <juandaniel200031@gmail.com>
2021-10-24 01:57:25 -0500

    Design continuation

--- frontend/src/Components/LoginComponents.js
+++ frontend/src/Components/LoginComponents.js
@@ -16,36 +16,24 @@ export default function LoginComponents() {
   const [Contraseña, setContraseña] = useState("");
   const [Correo, setCorreo] = useState("");
   const [ConfiContraseña, setConfiContraseña] = useState("");
-  
   const [Teléfono, setTeléfono] = useState("");
-  
   const[InLogin,setLogin] = React.useState(true);
   const [InRegistro, setRegistro] = React.useState(false);
-
   const handleOpenRegistro = () => (setRegistro(true),setLogin(false))
   const handleCloseregistro = () => setRegistro(false);
- 
-  
   const handleOpenLogin = () => (setRegistro(false),setLogin(true))
-  
-    
   const handleCloselogin = () => setLogin(false);
-  
   const joinRoom = () => {
     if (CorreoLog !== "" && ContraseñaLog !== "") {
       socket.emit("Clave", ContraseñaLog);
-      
       setShowChat(true);
     }
   };
-
   const onSubmitLogin = async e => {
     await socket.emit('/api/Usuarios/Login',{Correo:CorreoLog,Contraseña:ContraseñaLog})
-    setShowChat(true)
-    
-   }
-  useEffect(() => {
-    
+    setShowChat(true)  
+  }
+  useEffect(() => { 
     socket.on("mensaje", (data) => {
       localStorage.setItem('success',data);
       const tokenString = localStorage.getItem('success');
@@ -56,152 +44,101 @@ export default function LoginComponents() {
       }
       else{
         localStorage.setItem('message',data.message);
-      const tokenString = localStorage.getItem('message');
+        const tokenString = localStorage.getItem('message');
         swal({timer: 3000, closeOnClickOutside: false,text:data.message,icon: "info" }).then(willDelete => {
-          if (willDelete) {
-            window.location.href="/";
-          }
-          else{window.location.href="/";}
-        });;
-        
+        if (willDelete) {
+          window.location.href="/";
+        }else{window.location.href="/";}
+        });
        }
     })
-}, [socket]);
- 
+  }, [socket]);
  const onSubmit= async e => {
   const res = await socket.emit('/api/Usuarios', { Nombre: Nombre, Teléfono:Teléfono, Contraseña:Contraseña, Correo:Correo, ConfiContraseña:ConfiContraseña },{
     method:'POST', headers:{
       'Accept':'application/json',
       'Content-Type':'application/json',
     }})
-
   }
-
-    return (
+  return (
+    <div>
+      {!showChat ? (
       <div>
-          {!showChat ? (
-            <div>
-          <Modal  open={InLogin} onClose={handleCloselogin}>  
-        <div class="box-form">
-           
-          <div className="modal-container">
-     
-    </div>
-  <div class="left">
-		<div class="overlay">
-
-		</div>
-	</div>
-  
-    
- <div class="right "  >
-		<h5>Iniciar sesión</h5>
-        <form  onSubmit={onSubmitLogin} >
-                  <div className="group col-md-12" >
-                    <label htmlFor="email" className="label ">Correo</label>
-                    <input  onChange={(event) => {setUsername(event.target.value);}} required="required" placeholder="Email@gmail.com" id="Email" name="Email" type="email" className="input"/>
-                  </div>
-                
+        <Modal  open={InLogin} onClose={handleCloselogin}>  
+          <div class="box-form">   
+            <div className="modal-container"></div>
+            <div class="left">
+              <div class="overlay"></div>
+            </div>
+            <div class="right "  >
+              <h5>Iniciar sesión</h5>
+              <form  onSubmit={onSubmitLogin} >
+                <div className="group col-md-12" >
+                  <label htmlFor="email" className="label ">Correo</label>
+                  <input  onChange={(event) => {setUsername(event.target.value);}} required="required" placeholder="Email@gmail.com" id="Email" name="Email" type="email" className="input"/>
+                </div>
+                <br/>        
+                <div className="group col-md-12">
+                  <label htmlFor="password" className="label ">Contraseña</label>
+                  <input  onChange={(event) => {
+                    setRoom(event.target.value);
+                    }} required="required" placeholder="Ingresa tu contraseña" id="password" name="password" type="password" className="input"
+                  />
+                </div>
+                <br></br><br/>
+                <button type="submit" class="button2" >Iniciar sesión</button>
+                <button onClick={handleOpenRegistro} class="button2" >Registro</button>
+              </form>              
+            </div>    
+          </div>  
+        </Modal> 
+        <Modal  open={InRegistro} onClose={handleCloseregistro}>  
+          <div class="box-form">     
+            <div className="modal-container">
+          </div>
+          <div class="left">
+            <div class="overlay"></div>
+          </div>
+          <div class="right ">
+            <h5>Registro</h5>
+              <form  onSubmit={onSubmit} >
+                <div className="group col-md-12" >
+                  <label htmlFor="Nombre" className="label ">Nombre</label>
+                  <input onChange={(event) => {setNombre(event.target.value);}} placeholder="Ingresa tu Nombre"  required="required" autofocus="autofocus" id="Nombre" name="Nombre" type="text" className="input"/>
+                </div>
                 <br/>
-                  
-                  <div className="group col-md-12">
-                    <label htmlFor="password" className="label ">Contraseña</label>
-                    <input  onChange={(event) => {
-              setRoom(event.target.value);
-            }} required="required" placeholder="Ingresa tu contraseña" id="password" name="password" type="password" className="input"/>
-                  </div>
-                  <br></br>
-                
-                  <br/>
-			
-		
-			
-			
-                  <button type="submit" class="button2" >
-				            Iniciar sesión
-                </button>
-                <button onClick={handleOpenRegistro} class="button2" >
-				            Registro
-                </button>
-                </form>      
-              
-        </div>
-        
-        
-      </div>
-      
-      </Modal> 
-
-
-      <Modal  open={InRegistro} onClose={handleCloseregistro}>  
-        <div class="box-form">     
-          <div className="modal-container">
-        </div>
-        <div class="left">
-		      <div class="overlay">
-    		  </div>
-	      </div>
-    
-        <div class="right "  >
-          <h5>Registro</h5>
-            <form  onSubmit={onSubmit} >
-              <div className="group col-md-12" >
-                <label htmlFor="Nombre" className="label ">Nombre</label>
-                <input onChange={(event) => {setNombre(event.target.value);}} placeholder="Ingresa tu Nombre"  required="required" autofocus="autofocus" id="Nombre" name="Nombre" type="text" className="input"/>
-              </div>
-              <br/>
-                  
-              <div className="group col-md-12">
-                <label htmlFor="password" className="label ">Correo</label>
-                <input  onChange={(event) => {setCorreo(event.target.value);}} required="required" placeholder="Email@gmail.com" id="Email" name="Email" type="email" className="input"/>
-              </div>
-              <br/>
-              <div className="group col-md-12" >
-                <label htmlFor="Contraseña" className="label ">Contraseña</label>
-                <input onChange={(event) => {setContraseña(event.target.value);}} placeholder="Ingresa tu Contraseña"  required="required"  id="Contraseña" name="Contraseña" type="password" className="input"/>
-              </div>
-              <br/>
-              <div className="group col-md-12" >
-                <label htmlFor="ConfiContraseña" className="label ">Confirmar contraseña</label>
-                <input onChange={(event) => {setConfiContraseña(event.target.value);}} placeholder="Ingresa tu Contraseña"  required="required"  id="ConfiContraseña" name="ConfiContraseña" type="password" className="input"/>
-              </div>
-              <br/>
-              <div className="group col-md-12" >
-                <label htmlFor="Teléfono" className="label ">Teléfono</label>
-                <input onChange={(event) => {setTeléfono(event.target.value);}} placeholder="Ingresa el numero del teléfono"  required="required"  id="Teléfono" name="Teléfono" type="number" className="input"/>
-              </div>
-                
-                
+                <div className="group col-md-12">
+                  <label htmlFor="password" className="label ">Correo</label>
+                  <input  onChange={(event) => {setCorreo(event.target.value);}} required="required" placeholder="Email@gmail.com" id="Email" name="Email" type="email" className="input"/>
+                </div>
                 <br/>
-                  
-             <br></br>
-                
-                  <br/>
-			
-		
-			
-			
-                  <button onClick={handleOpenLogin} class="button2" >
-				            Iniciar sesión
-                </button>
+                <div className="group col-md-12" >
+                  <label htmlFor="Contraseña" className="label ">Contraseña</label>
+                  <input onChange={(event) => {setContraseña(event.target.value);}} placeholder="Ingresa tu Contraseña"  required="required"  id="Contraseña" name="Contraseña" type="password" className="input"/>
+                </div>
+                <br/>
+                <div className="group col-md-12" >
+                  <label htmlFor="ConfiContraseña" className="label ">Confirmar contraseña</label>
+                  <input onChange={(event) => {setConfiContraseña(event.target.value);}} placeholder="Ingresa tu Contraseña"  required="required"  id="ConfiContraseña" name="ConfiContraseña" type="password" className="input"/>
+                </div>
+                <br/>
+                <div className="group col-md-12" >
+                  <label htmlFor="Teléfono" className="label ">Teléfono</label>
+                  <input onChange={(event) => {setTeléfono(event.target.value);}} placeholder="Ingresa el numero del teléfono"  required="required"  id="Teléfono" name="Teléfono" type="number" className="input"/>
+                </div>
+                <br/><br></br><br/>
+                <button onClick={handleOpenLogin} class="button2" >Iniciar sesión</button>
                 <button type="submit"  class="button2" >
-				            Enviar
+                    Enviar
                 </button>
-                </form>      
-              
-        </div>
-        
-        
-      </div>
-      
-      </Modal> 
+              </form>      
+            </div>
+          </div>
+        </Modal> 
       </div>  
       ) : (
-            <SalaComponents socket={socket} CorreoLog={CorreoLog} ContraseñaLog={ContraseñaLog} />
-          )}
-           
-
-         
-      </div>
-    )
+        <SalaComponents socket={socket} CorreoLog={CorreoLog} ContraseñaLog={ContraseñaLog} />
+      )}
+    </div>
+  )
 }
--- frontend/src/Components/SalaComponents.js
+++ frontend/src/Components/SalaComponents.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
-import swal from 'sweetalert';
+//import swal from 'sweetalert';
 import "./css/sala.css"
 import ViewUser from './ViewUser';
 import user from '../assets/user.png';
 import contact from '../assets/contact.png';
-
+import send from '../assets/send.svg';
+import down_menu from '../assets/down-menu.svg'
 function SalaComponents({ socket, CorreoLog, ContraseñaLog }) {
     const [currentMessage, setCurrentMessage] = useState("");
     const [messageList, setMessageList] = useState([]);
@@ -39,7 +40,11 @@ function SalaComponents({ socket, CorreoLog, ContraseñaLog }) {
           imgPerfil={user} 
           name="Alejandro García"
           phone="55 2026 0240"
-        />
+        >
+          <div className="text-chat">
+            <p>Chat</p>
+          </div>
+        </ViewUser>
       </div>
       <div className="chat-window">
         <div className="chat-header">
@@ -48,24 +53,54 @@ function SalaComponents({ socket, CorreoLog, ContraseñaLog }) {
         </div>
         <div className="chat-body">
           <ScrollToBottom className="message-container">
-            {messageList.map((messageContent) => {
-              return (
-                <div
-                  className="message"
-                  id={CorreoLog === messageContent.author ? "you" : "other"}
-                >
-                  <div>
-                    <div className="message-content">
-                      <p>{messageContent.message}</p>
-                    </div>
-                    <div className="message-meta">
-                      <p id="time">{messageContent.time}</p>
-                      <p id="author">{messageContent.author}</p>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            <div className="message-you">
+              <div className="message__img-user-you">
+                <img src={contact} alt="Imagen de la persona que lo envío" />
+              </div>
+              <div className="message__text-you">
+                <p className="message__text--send-you">
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                eeeeeeeeeeeeeeeeeee
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                eeeeeeeeeeeeeeeeeee
+                </p>
+              </div>
+            </div>
+            <div className="message-you">
+              <div className="message__img-user-you">
+                <img src={contact} alt="Imagen de la persona que lo envío" />
+              </div>
+              <div className="message__text-you">
+                <p className="message__text--send-you">
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                eeeeeeeeeeeeeeeeeee
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                texto de mensajetexto de mensaje
+                eeeeeeeeeeeeeeeeeee
+                </p>
+              </div>
+            </div>
+
+            <div className="message-another">
+              <div className="message__text-another">
+                <p className="message__text--send-another">
+                  texto de mensajetexto de mensaje
+                </p>
+                <p className="message__text--send-another">
+                  texto de mensajetexto de mensaje
+                </p>
+              </div>
+              <div className="message__img-user-another">
+                <img src={user} alt="Imagen de la persona que lo envío" />
+              </div>
+            </div>
           </ScrollToBottom>
         </div>
         <div className="chat-footer">
@@ -80,13 +115,17 @@ function SalaComponents({ socket, CorreoLog, ContraseñaLog }) {
               event.key === "Enter" && sendMessage();
             }}
           />
-          <button onClick={sendMessage}>&#9658;</button>
+          <button className="btn-send-message" onClick={sendMessage}><img className="icon-btn-send" src={send} alt="Icono de envíar" /></button>
         </div>
       </div>
       <div className="main-container__user">
         <ViewUser imgPerfil={contact} name="Lucía González" phone="55271273617">
           <div className="container__user-btn-edit">
             <button className="btn-edit-data">Editar datos</button>
+          </div>
+          <div className="contact-data">
+            <p>Datos del contacto</p>
+            <img src={down_menu} alt="Icono de menú abajo o arriba" />
           </div>
         </ViewUser>
       </div>
